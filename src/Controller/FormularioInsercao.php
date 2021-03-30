06403ced300b0895e03347e380a7b72cd4c3a8e1
Carlos Accyole <carlos_accyole@hotmail.com>
2021-03-29 22:49:38 -0300

    implementando PSR 7, 17 e 15

---
+++ src/Controller/FormularioInsercao.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class FormularioInsercao implements RequestHandlerInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $html = 'Teste';
+        return new Response(200, [], $html);
+    }
+}
